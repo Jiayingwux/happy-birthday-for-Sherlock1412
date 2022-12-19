0b23ed8f772fd42c8b5ecc7a8be56c8f95aa0686
Jiayingwux <120944933+Jiayingwux@users.noreply.github.com>
2022-12-19 21:22:25 +0800

    Update config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ var config = {
      */
     imgs: {
         "送给贝贝": "./imgs/xiaokei.jpg",
-        "...⭐...⭐...⭐...": "./imgs/xuyuan.jpg",
+       //"...⭐...⭐...⭐...": "./imgs/xuyuan.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
